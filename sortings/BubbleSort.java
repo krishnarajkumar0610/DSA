d26e4d8bda041e163ab58e2f14ce2e75a570d6f2
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-18 22:08:41 +0530

    BubbleSort

--- sortings/BubbleSort.java
+++ sortings/BubbleSort.java
@@ -1,10 +1,12 @@
 package dsa.DSA.sortings;
 
+import dsa.DSA.sortings.special_methods.SpecialMethods;
+
 import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        bubbleSort(ArrayValue.generateRandomArrayValues());
+        bubbleSort(SpecialMethods.generateRandomArrayValues());
     }
 
     private static void bubbleSort(int[] values) {
@@ -12,7 +14,7 @@ public class BubbleSort {
             boolean isSwapped = false;
             for (int j = 0; j < values.length - i - 1; j++) {
                 if (values[j] > values[j + 1]) {
-                    Swapping.swap(values, j, j + 1); // calling user defined swap method
+                    SpecialMethods.swap(values, j, j + 1); // calling user defined swap method
                     isSwapped = true;
                 }
             }
