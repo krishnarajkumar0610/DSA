85c3722267b3a79c76e951b6879548cc02e60451
krishnarajkumar0610 <krishnaraj0610@gmail.com>
2024-06-18 22:04:54 +0530

    Created swapping method

--- sortings/BubbleSort.java
+++ sortings/BubbleSort.java
@@ -9,11 +9,16 @@ public class BubbleSort {
 
     private static void bubbleSort(int[] values) {
         for (int i = 0; i < values.length; i++) {
+            boolean isSwapped = false;
             for (int j = 0; j < values.length - i - 1; j++) {
                 if (values[j] > values[j + 1]) {
-                    Swaping.swap(values, j, j + 1); // calling user defined swap method
+                    Swapping.swap(values, j, j + 1); // calling user defined swap method
+                    isSwapped = true;
                 }
-                System.out.println("Pass " + (i + 1) + ": " + Arrays.toString(values));
+            }
+            System.out.println("Pass " + (i + 1) + ":" + Arrays.toString(values));
+            if (!isSwapped) {
+                break;
             }
             System.out.println();
         }
